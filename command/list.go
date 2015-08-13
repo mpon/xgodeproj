eed4204daaeab402c560733842ab574bf1674410
Masato Ohshima <m_ohshima@r.recruit.co.jp>
2015-08-14 00:26:25 +0900

    [add] read file

--- command/list.go
+++ command/list.go
@@ -1,7 +1,31 @@
 package command
 
-import "github.com/codegangsta/cli"
+import (
+	"bufio"
+	"fmt"
+	"os"
 
+	"github.com/codegangsta/cli"
+)
+
+// CmdList prints list
 func CmdList(c *cli.Context) {
-	// Write your code here
+
+	var fp *os.File
+	var err error
+
+	fp, err = os.Open(c.Args()[0])
+
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
 }
