d432239062cb7af15fc3a6266e0002e6f2c0a00e
Masato Ohshima <m_ohshima@r.recruit.co.jp>
2015-08-14 17:20:29 +0900

    [add] convert json and read json

--- command/list.go
+++ command/list.go
@@ -1,36 +1,72 @@
 package command
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
-	"strings"
+	"os/exec"
 
+	"github.com/bitly/go-simplejson"
 	"github.com/codegangsta/cli"
 )
 
 // CmdList prints list
 func CmdList(c *cli.Context) {
 
-	var fp *os.File
 	var err error
 
-	fp, err = os.Open(c.Args()[0])
+	// plutil -convert json -o tmp.json -r project.pbxproj
+	json := "tmp.json"
+	cmd := exec.Command("plutil", "-convert", "json", "-o", json, "-r", c.Args()[0])
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 
+	// read File to byte type
+	rf, err := ioutil.ReadFile(json)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	scanner := bufio.NewScanner(fp)
-	for scanner.Scan() {
-		t := scanner.Text()
-		if strings.Contains(t, "/* Begin") {
-			fs := strings.Fields(t)
-			fmt.Println(fs[2])
+	// convert []byte type to json type
+	js, err := simplejson.NewJson(rf)
+
+	// get classes
+	printClasses(js)
+	printString(js, "objectVersion")
+	printString(js, "archiveVersion")
+	printString(js, "rootObject")
+	printObjects(js)
+
+	// temp file removed
+	os.Remove(json)
+
+}
+
+func printClasses(js *simplejson.Json) {
+	fmt.Println("======== classes =========")
+	cs := js.Get("classes").MustMap()
+	for k, v := range cs {
+		fmt.Println(k + " = ")
+		fmt.Println(v)
+	}
+}
+
+func printString(js *simplejson.Json, key string) {
+	fmt.Println("======== " + key + " =========")
+	v := js.Get(key).MustString()
+	fmt.Println(key + " = " + v)
+}
+
+func printObjects(js *simplejson.Json) {
+	fmt.Println("======== objects =========")
+	objects := js.Get("objects").MustMap()
+	for _, m := range objects {
+		for k, v := range m.(map[string]interface{}) {
+			if k == "isa" {
+				fmt.Println(k + " = " + v.(string))
+			}
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
-
 }
